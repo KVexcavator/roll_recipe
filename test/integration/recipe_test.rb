0942fd6b1c080448c0de55035c1462e2e643d359
kv-2 <str_token@mail.ru>
2019-06-22 18:05:38 +0300

    add test "create recipes"

--- test/integration/recipe_test.rb
+++ test/integration/recipe_test.rb
@@ -1,9 +1,24 @@
+
 require 'test_helper'
-# load the test record and navigate to the new recipe page
-curry = recipes(:curry)
-get "/recipes/new"
-# post the data to the new method and assert for a success response
-assert_response :success
-post_via_redirect "/recipes/new", title: recipes(:curry).title
+
+class RecipeFlowsTest < ActionDispatch::IntegrationTest
+  fixtures :recipes
+  test "create recipes" do
+    https!
+    # load the test record and navigate to the new recipe page
+    curry = recipes(:curry)
+    get "/recipes/new"
+    # post the data to the new method and assert for a success response
+    assert_response :success
+    post_via_redirect "/recipes/new", title: recipes(:curry).title
+
+    assert_equal '/recipes', path
+    assert_equal 'Create Recipe', flash[:notice]
+    https!(false)
+    get "/recipes"
+    assert_response :success
+    assert assigns(:recipes)
+  end
+end
 
 
