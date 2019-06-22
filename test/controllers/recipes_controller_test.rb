a06da70ea0bead2ea39bc1f3cbda365098a50529
kv-2 <str_token@mail.ru>
2019-06-22 19:54:12 +0300

    add test "should get edit"

--- test/controllers/recipes_controller_test.rb
+++ test/controllers/recipes_controller_test.rb
@@ -18,15 +18,17 @@ class RecipesControllerTest<ActionController::TestCase
 
   test "should create recipe" do
     assert_difference('Recipe.count') do
-      post :create, recipe: { cooking_time:
-      @recipe.cooking_time, cuisine_id:
-      @recipe.cuisine_id, food_preference_id:
-      @recipe.food_preference_id, food_type:
-      @recipe.food_type, ingredients:
-      @recipe.ingredients, level_of_difficulty:
-      @recipe.level_of_difficulty, procedure:
-      @recipe.procedure, servings: @recipe.servings,
-      title: @recipe.title }
+      post :create, recipe: { 
+        cooking_time: @recipe.cooking_time,
+        cuisine_id: @recipe.cuisine_id,
+        food_preference_id: @recipe.food_preference_id,
+        food_type: @recipe.food_type,
+        ingredients: @recipe.ingredients,
+        level_of_difficulty: @recipe.level_of_difficulty,
+        procedure: @recipe.procedure,
+        servings: @recipe.servings,
+        title: @recipe.title 
+      }
     end
     assert_redirected_to recipe_path(assigns(:recipe))
   end
@@ -36,6 +38,9 @@ class RecipesControllerTest<ActionController::TestCase
     assert_response :success
   end
 
-
+  test "should get edit" do
+    get :edit, id: @recipe
+    assert_response :success
+  end
 end
 
