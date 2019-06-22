91483ae170acb719382403bea2190a039507fbd6
kv-2 <str_token@mail.ru>
2019-06-22 19:44:01 +0300

    add test "should create recipe"

--- test/controllers/recipes_controller_test.rb
+++ test/controllers/recipes_controller_test.rb
@@ -15,4 +15,20 @@ class RecipesControllerTest<ActionController::TestCase
     get :new
     assert_response :success
   end
+
+  test "should create recipe" do
+    assert_difference('Recipe.count') do
+      post :create, recipe: { cooking_time:
+      @recipe.cooking_time, cuisine_id:
+      @recipe.cuisine_id, food_preference_id:
+      @recipe.food_preference_id, food_type:
+      @recipe.food_type, ingredients:
+      @recipe.ingredients, level_of_difficulty:
+      @recipe.level_of_difficulty, procedure:
+      @recipe.procedure, servings: @recipe.servings,
+      title: @recipe.title }
+    end
+    assert_redirected_to recipe_path(assigns(:recipe))
+  end
 end
+
