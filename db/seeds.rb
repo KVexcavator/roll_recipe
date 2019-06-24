f2655d7404776c1e55e3a8cb5b04aa5f624c6575
kv-2 <str_token@mail.ru>
2019-06-24 15:55:10 +0300

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,5 +2,5 @@
 
 #default data to food_type after db:seeds  the "creae" need revemp by the "first_or_create"
 food_types = ["Curry", "Dessert", "Sides", "Breakfast"]
-food_types.each{|d| FoodType.where(:name => d).create}
+food_types.each{|d| FoodType.where(:name => d).first_or_create}
 
