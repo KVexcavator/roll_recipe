c7912a57510e2d8ec45972db9af8665873e769cd
kv-2 <str_token@mail.ru>
2019-06-27 21:02:32 +0300

    add card

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -1,2 +1,8 @@
 module RecipesHelper
+  #helper_method :sidebar_values
+  def sidebar_values
+    @food_preferences = FoodPreference.all
+    @food_types = FoodType.all
+    @cuisines = Cuisine.all
+  end
 end
